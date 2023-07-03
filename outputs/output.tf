e6ec4ab10ce4e9c878d0f1b3e463f4fefea96ef6
sairam <sairm21@gmail.com>
2023-07-03 20:12:10 +0530

    terraform output code

---
+++ outputs/output.tf
@@ -0,0 +1,10 @@
+output "sample1" {
+  value       = "strings in double quotes"
+}
+
+output "sample2" {
+  value       = true
+  value = true
+}
+
+# numbers are booleans(true and false) need not be in quotes
