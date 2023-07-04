88fd6da1b48541d89512586c4f7c98fa7f5db13a
sairam <sairm21@gmail.com>
2023-07-04 22:04:05 +0530

    terraform data code

--- data-sources/data.tf
+++ data-sources/data.tf
@@ -1,20 +1,20 @@
-data "aws_ec2_instance_spot_price" "example" {
-  instance_type     = "t3.medium"
-  availability_zone = "us-west-2a"
-
-  filter {
-    name   = "product-description"
-    values = ["Linux/UNIX"]
-  }
-}
-
-output "price" {
-  value = data.aws_ec2_instance_spot_price.example.instance_type
-}
-
-output "price1" {
-  value = data.aws_ec2_instance_spot_price.example.availability_zone
-}
+#data "aws_ec2_instance_spot_price" "example" {
+#  instance_type     = "t3.medium"
+#  availability_zone = "us-west-2a"
+#
+#  filter {
+#    name   = "product-description"
+#    values = ["Linux/UNIX"]
+#  }
+#}
+#
+#output "price" {
+#  value = data.aws_ec2_instance_spot_price.example.instance_type
+#}
+#
+#output "price1" {
+#  value = data.aws_ec2_instance_spot_price.example.availability_zone
+#}
 
 data "aws_security_group" "selected" {
   name = "allow-all"
@@ -26,4 +26,47 @@ output "sg" {
 
 output "sg1" {
   value = data.aws_security_group.selected.vpc_id
+}
+
+data "aws_pricing_product" "example" {
+  service_code = "AmazonEC2"
+
+  filters {
+    field = "instanceType"
+    value = "c5.xlarge"
+  }
+
+  filters {
+    field = "operatingSystem"
+    value = "Linux"
+  }
+
+  filters {
+    field = "location"
+    value = "US East (N. Virginia)"
+  }
+
+  filters {
+    field = "preInstalledSw"
+    value = "NA"
+  }
+
+  filters {
+    field = "licenseModel"
+    value = "No License required"
+  }
+
+  filters {
+    field = "tenancy"
+    value = "Shared"
+  }
+
+  filters {
+    field = "capacitystatus"
+    value = "Used"
+  }
+}
+
+output "instance" {
+  value = data.aws_pricing_product.example
 }
