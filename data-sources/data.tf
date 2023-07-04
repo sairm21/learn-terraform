85bd2e46fdf7fed779aeb977ac44c1b7ab2ea26e
sairam <sairm21@gmail.com>
2023-07-04 21:28:18 +0530

    terraform data code

--- data-sources/data.tf
+++ data-sources/data.tf
@@ -1,13 +1,25 @@
-data "aws_ec2_instance_spot_price" "example" {
-  instance_type     = "t3.medium"
-  availability_zone = "us-west-2a"
+#data "aws_ec2_instance_spot_price" "example" {
+#  instance_type     = "t3.medium"
+#  availability_zone = "us-west-2a"
+#
+#  filter {
+#    name   = "product-description"
+#    values = ["Linux/UNIX"]
+#  }
+#}
+#
+#output "price" {
+#  value = data.aws_ec2_instance_spot_price.example.spot_price
+#}
 
-  filter {
-    name   = "product-description"
-    values = ["Linux/UNIX"]
-  }
+data "aws_security_group" "selected" {
+  name = "allow-all"
 }
 
-output "price" {
-  value = data.aws_ec2_instance_spot_price.example.spot_price
+output "sg" {
+  value = data.aws_security_group.selected.id
+}
+
+output "sg1" {
+  value = data.aws_security_group.selected.vpc_id
 }
