b78c22d93d10e56997b43519875043c7ac1a0a7a
sairam <sairm21@gmail.com>
2023-07-19 21:47:17 +0530

    tf resource block

---
+++ resource/main.tf
@@ -0,0 +1,14 @@
+data "aws_ami" "example4" {
+  most_recent      = true
+  name_regex       = "Centos-8-DevOps-Practice"
+  owners           = ["973714476881"]
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.example4.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
