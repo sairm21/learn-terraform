f5bf49ff2794686694ee1645dd9c185cea227d39
sairam <sairm21@gmail.com>
2023-07-22 11:18:24 +0530

    provisioners demo

---
+++ provisioner/main.tf
@@ -0,0 +1,29 @@
+data "aws_ami" "example" {
+  most_recent      = true
+  name_regex       = "Centos-8-DevOps-Practice"
+  owners           = ["973714476881"]
+}
+
+resource "aws_instance" "web" {
+  ami                   = data.aws_ami.example4.id
+  instance_type         = "t3.micro"
+  vpc_security_group_id = ["sg-07010737da7cff878"]
+
+  tags = {
+    Name = hello world
+  }
+
+  connection {
+    type     = "ssh"
+    user     = "centos"
+    password = "DevOps321"
+    host     = self.public_ip
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo labauto ansible",
+      "ansible-pull -i localhost, -U https://github.com/sairm21/roboshop-ansible main.yml -e role_name=Frontend",
+    ]
+  }
+}
