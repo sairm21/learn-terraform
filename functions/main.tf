45643ed1f77df93aa4dfb8c8f9848a72e11e6228
sairam <sairm21@gmail.com>
2023-07-23 12:40:38 +0530

    functions

--- functions/main.tf
+++ functions/main.tf
@@ -14,3 +14,22 @@ output "fruits_count" {
   value = length(var.fruits)
 }
 
+variable "classes" {
+  default = {
+    devops={
+      name="devops"
+      topics=["jenkins", "docker"]
+    }
+    aws={
+      name="aws"
+    }
+  }
+}
+
+output "devops_topics" {
+  value = var.classes["devops"]["topics"]
+}
+
+output "aws_topics" {
+  value = lookup(lookup(var.classes, "aws", null), "topics", "no topic so far")
+}
