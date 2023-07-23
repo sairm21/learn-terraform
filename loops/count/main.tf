9e57af8e5d17dff8e6cee7735baac349ba1932c5
sairam <sairm21@gmail.com>
2023-07-23 10:37:13 +0530

    loops count loop

---
+++ loops/count/main.tf
@@ -0,0 +1,16 @@
+data "aws_ami" "example" {
+  most_recent      = true
+  name_regex       = "Centos-8-DevOps-Practice"
+  owners           = ["973714476881"]
+}
+
+resource "aws_instance" "web" {
+  count = 3
+  ami           = data.aws_ami.example.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
