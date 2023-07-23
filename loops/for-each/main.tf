d55bcbdbbd0a33693eb6bebca7efbf6c3fd9d30b
sairam <sairm21@gmail.com>
2023-07-23 15:47:03 +0530

    instances creation according to length variable

---
+++ loops/for-each/main.tf
@@ -0,0 +1,35 @@
+data "aws_ami" "example" {
+  most_recent      = true
+  name_regex       = "Centos-8-DevOps-Practice"
+  owners           = ["973714476881"]
+}
+
+resource "aws_instance" "web" {
+  count = length(var.instances)
+  ami           = data.aws_ami.example.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = element(var.instances, count.index)
+  }
+}
+
+variable "instances" {
+  default = {
+
+    frontend={
+      name="frontend"
+      instance_type="t3.micro"
+    }
+    catalogue={
+      name="catalogue"
+      instance_type="t3.small"
+    }
+    cart={
+      name="cart"
+      instance_type="t3.nano"
+    }
+
+  }
+}
+
