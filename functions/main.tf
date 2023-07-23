47d22390ff190647d566dac5b6132f81dfc47586
sairam <sairm21@gmail.com>
2023-07-23 12:14:34 +0530

    functions

--- functions/main.tf
+++ functions/main.tf
@@ -6,3 +6,11 @@ output "class" {
   value = upper(var.class)
 }
 
+variable "fruits" {
+  default = ["apple", "bannana", "orange"]
+}
+## print the same in capital letters
+output "fruits_count" {
+  value = length(var.fruits)
+}
+
