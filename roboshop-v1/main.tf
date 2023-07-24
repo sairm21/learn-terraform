969919db84160412ee3901b73b10403531c919d2
sairam <sairm21@gmail.com>
2023-07-24 22:24:25 +0530

    roboshop v1 with modules

--- roboshop-v1/main.tf
+++ roboshop-v1/main.tf
@@ -1,54 +1,22 @@
-module "Cart" {
+module "instances" {
+  for_each = var.instances
   source = "./ec2"
-  name= "Cart"
+  name= each.key
 }
 
-module "Catalogue" {
-  source = "./ec2"
-  name= "Catalogue"
+variable "instances" {
+  default={
+    Frontend={}
+    MongoDB={}
+    Catalogue={}
+    Redis={}
+    User={}
+    Cart={}
+    MySQL={}
+    Shipping={}
+    RabbitMQ={}
+    Payment={}
+    Dispatch={}
+  }
 }
 
-module "Common" {
-  source = "./ec2"
-  name= "Common"
-}
-
-module "Frontend" {
-  source = "./ec2"
-  name= "Frontend"
-}
-
-module "MongoDB" {
-  source = "./ec2"
-  name= "MongoDB"
-}
-
-module "MySQL" {
-  source = "./ec2"
-  name= "MySQL"
-}
-
-module "Payment" {
-  source = "./ec2"
-  name= "Payment"
-}
-
-module "RabbitMQ" {
-  source = "./ec2"
-  name= "RabbitMQ"
-}
-
-module "Redis" {
-  source = "./ec2"
-  name= "Redis"
-}
-
-module "Shipping" {
-  source = "./ec2"
-  name= "Shipping"
-}
-
-module "User" {
-  source = "./ec2"
-  name= "User"
-}
